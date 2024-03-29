abfaae2f0d42b8774c02f85f47f00b660c36a66e
KevalDave501 <163372246+KevalDave501@users.noreply.github.com>
2024-03-29 11:27:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const sequelize = require("./utils/Database");
+const employeesRoutes = require("./routes/employeesRoutes");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  "/public/assets",
+  express.static(path.join(__dirname, "public", "assets"))
+);
+
+app.use(employeesRoutes);
+
+sequelize;
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
